4e3a5ac9389f171f9f31377cb37139d1b6dc11b3
lesmul <lesmul@wp.pl>
2022-12-01 12:36:40 +0100

    Wersja 5 1/12/2022

--- DBDT/DrzewoProcesu/Directory/DirectoryStructure.cs
+++ DBDT/DrzewoProcesu/Directory/DirectoryStructure.cs
@@ -23,7 +23,7 @@ namespace DBDT
             }
             else
             {
-                return Directory.GetLogicalDrives().Select(drive => new DirectoryItem { FullPath = lokalizacja_start, Type = DirectoryItemType.Folder }).ToList();
+                return Directory.GetLogicalDrives().Select(drive => new DirectoryItem { FullPath = lokalizacja_start, Type = DirectoryItemType.Drive }).Where(drive => drive.Name == lokalizacja_start).ToList();
             }
 
         }
